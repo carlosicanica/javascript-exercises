5ca6b02153c5a33280abeb3b8421aaafb9c03e6b
carlosicanica <carlosicanica@gmail.com>
2023-04-26 19:36:49 +0200

    add repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+let string;
+let num;
+
+const repeatString = function(string, num) {
+    let finalString = '';
+    while (num > 0) {
+        finalString = finalString + string;
+        num--;
+    }
+    
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        return finalString;
+
+    }
 
 };
+
+repeatString('carlo', 5);
 
 // Do not edit below this line
 module.exports = repeatString;
