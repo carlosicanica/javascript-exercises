1b87ab84e44ed4e0e0395523b50dd8517ce1d55e
carlosicanica <carlosicanica@gmail.com>
2023-04-26 19:53:21 +0200

    add reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+let string;
+
+const reverseString = function(string) {
+    let splitWordsArray = string.split("");
+    let reverseArray = splitWordsArray.reverse();
+    let finalString = reverseArray.join("");
+    return finalString;
+    
 
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
